09af51920c938fdfe2a0d5dc5358b6f550c0faea
n-ob2 <n_ob2@hotmail.com>
2021-10-20 14:53:53 +0800

    全データプッシュ

---
+++ app/src/main/java/com/example/tourlistsights/Slight.kt
@@ -0,0 +1,11 @@
+package com.example.tourlistsights
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+class Slight (
+    val name: String,
+    val imageName: String,
+    val descripttion: String,
+    val kind: String
+)
